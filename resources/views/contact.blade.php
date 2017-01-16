c63580dd32acea3c030174adf97d73fccc3ed66c
Matthew Thompson <matthew.thompson.a@gmail.com>
2017-01-16 14:30:33 -0500

    removed old Contact code

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,45 +1,8 @@
 @extends('layouts.main')
 @section('mainContent')
-  <h2 class="pageTitle">Contact</h2>
-
-  <div class="Contact">
-    <div class="contactWrap">
-      <a href="mailto:matthew.thompson.a@gmail.com?subject=3WireBuild-Contact" target="_blank">
-        <img class="contactIcon" src="linkImages/Social/gmail.png">
-    		<p class="contactDesc"><b>Email: </b>matthew.thompson.a@gmail.com</p>
-      </a>
-    </div>
-
-    <div class="contactWrap">
-      <a href="https://github.com/mthomps4" target="_blank">
-    		<img class="contactIcon" src="linkImages/Social/github.png">
-    		<p class="contactDesc"><b>GitHub:</b>@MThomps4</p>
-      </a>
-    </div>
-
-    <div class="contactWrap">
-      <a href="https://www.linkedin.com/in/mthomps4" target="_blank">
-        <img class="contactIcon" src="linkImages/Social/linkedin.png">
-    		<p class="contactDesc"><b>LinkedIn:</b> /in/mthomps4</p>
-      </a>
-    </div>
-
-    <div class="contactWrap">
-      <a href="https://www.instagram.com/3wirebuild/" target="_blank">
-        <img class="contactIcon" src="linkImages/Social/instagram.png">
-    		<p class="contactDesc"><b>Instagram:</b>@3WireBuild</p>
-      </a>
-    </div>
-  </div>
-
-<hr>
+  <h2 class="pageTitle">Contact Me<a href="mailto:matthew.thompson.a@gmail.com" target="_blank" class="aboutLinks"><img src="linkImages/Social/gmail.png" alt="Email Link"></a></h2>
 
   <div class="">
-    <div class="">
-      <img class="profile" src="/linkImages/img/profile.jpg" />
-    </div>
-
-
     <?php
       if (session()->has('data.name')){
         $name = session('data.name');
